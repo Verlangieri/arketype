2d8548e2ff38636ee9d1e3c22111d5dd6a337294
Verlangieri <verlangieri.david@gmail.com>
2016-02-17 16:42:18 +0100

    maj

--- client/templates/application/layout.js
+++ client/templates/application/layout.js
@@ -9,24 +9,33 @@ Template.layout.events = {
 
     /* OUVERTURE BOITE MODAL */
     if (link.attr('class') == "modal-link"){
-      console.log($(event.target));
+      //console.log($(event.target));
       $('#inner-modal').empty(); // vide l'intérieur de la div
-      var url = link.data('video'); // recupere l'attribut data
-      $('#inner-modal').append('<iframe id="loadedFrame" width="560" height="315" src="'+url+'" frameborder="0" allowfullscreen></iframe>') //l'insere dans la div inner modal
-      
-      document.getElementById('loadedFrame').onload = function() { // Lorsque mon iframe est chargée
+      var urlVideo = link.data('video'); // recupere l'attribut data
+      var urlImage = link.data('image'); // recupere l'attribut data
+
+      $('#modal').append('<span id="loaderModal">Chargement</span>')
+      if (urlVideo){
+        $('#inner-modal').append('<span id="inner-modal-close">Fermer</span><iframe id="loadedFrame" width="560" height="315" src="'+urlVideo+'" frameborder="0" allowfullscreen></iframe>') //l'insere dans la div inner modal
+      }
+      if (urlImage){
+        $('#inner-modal').append('<span id="inner-modal-close">Fermer</span><img id="loadedFrame" src="'+urlImage+'">') //l'insere dans la div inner modal
+      } 
+        // Lorsque mon iframe est chargée
         function applyBlur() {
           TweenMax.set($(".wrapper"), {webkitFilter:"blur(" + blurElement.a + "px)",filter:"blur(" + blurElement.a + "px)"});
         };
         TweenMax.set(modal, {display:'block',delay:0.5});
         TweenMax.set(innerModal, {autoAlpha: 0, scale: 0});
         TweenMax.to(blurElement, 0.5, {a:6, onUpdate:applyBlur}); // 0.3s for 6px blur
-        TweenMax.to(innerModal, 0.5, {autoAlpha: 1, scale: 1, ease: Power1.easeOut, delay:0.4});
-      };
+        document.getElementById('loadedFrame').onload = function() {
+          //$("#loaderModal").remove();
+          TweenMax.to(innerModal, 0.5, {autoAlpha: 1, scale: 1, ease: Power1.easeOut, delay:0.4});
+        };
     }
     
     /* FERMETURE BOITE MODAL */
-    if(event.target.id == "outer-modal"){
+    if(event.target.id == "outer-modal" || event.target.id == "inner-modal-close"){
       blurElement = {a:6};
 
       function removeBlur() {
@@ -113,11 +122,11 @@ Template.layout.rendered = function() {
           Meteor.setInterval(compteurInterval,100);
         }
 
-
-        //$('#message-compteur')
+        TweenMax.set($('.wrapper'), {autoAlpha: 1});
         TweenMax.set($('.header'), {scale: 1.3});
         TweenMax.from($("#loading-message"), 0.8, {autoAlpha:0, ease: Power1.easeIn, delay: 0.5});
-        TweenMax.from($("#loading-header"), 0.8, {width: '0', ease: Power1.easeIn, delay: 1});
+        TweenMax.from($("#loading-header"), 1.3, {width: '0','padding-left': '190px', ease: Power1.easeIn, delay: 0.5});
+        TweenMax.to($(".container__logo-subtitle-middle"), 2.3, {width: '230px', ease: Power3.easeIn, delay: 1.4,});
         TweenMax.from($(".logo-title"), 1, {marginTop: '50px',autoAlpha: 0, ease: Power1.easeOut, delay: 2});
         TweenMax.from($(".logo-subtitle"), 1, {autoAlpha: 0, ease: Power1.easeOut, delay: 2.5});
         TweenMax.to($("#loading-message"), 0.5, {autoAlpha:0, marginTop: '25px', delay: 3.5});
@@ -125,8 +134,9 @@ Template.layout.rendered = function() {
           $('#loading-header').removeAttr('id');
           TweenMax.set($('.header'), {autoAlpha: 1,width: '100%',scale: 1});
           TweenMax.from($(".header"), 1, {autoAlpha: 0,top: '-25px',ease: Power1.easeOut, delay: 0.3});
-
+          TweenMax.set($(".container__logo-subtitle-middle"),{width: '30px'});
         }});
         TweenMax.to($("#loading-page"), 0.5, {autoAlpha: 0, ease: Power1.easeIn, delay: 5});
+        TweenMax.from($(".wrapper-responsive"), 0.5, {autoAlpha: 0, ease: Power1.easeIn, delay: 5});
     }
 };
