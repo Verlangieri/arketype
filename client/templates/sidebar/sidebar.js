7a13a5ad3c3714bf75c60809105e77f9dcfc2ed3
Verlangieri <verlangieri.david@gmail.com>
2016-03-14 17:50:55 +0100

    corec

--- client/templates/sidebar/sidebar.js
+++ client/templates/sidebar/sidebar.js
@@ -18,6 +18,23 @@ Template.sidebar.rendered = function() {
       }
       timer();
 
+      //Pollution
+      function indicePollution(){
+        var selector = '.indicePollution';
+        function getRandomIntInclusive(min, max) {
+          return Math.floor(Math.random() * (max - min +1)) + min;
+        }
+        var compteurInterval = function(){
+          $( selector).empty();
+          $( selector+":eq(0)" ).append(getRandomIntInclusive(10, 15));
+          $( selector+":eq(1)" ).append(getRandomIntInclusive(345, 370));
+          $( selector+":eq(2)" ).append(getRandomIntInclusive(3, 5));
+          $( selector+":eq(3)" ).append(getRandomIntInclusive(5, 25));
+        }
+        Meteor.setInterval(compteurInterval,5000); 
+      }
+      indicePollution();
+
       function linksAnimation(){
 
         $('.sidebar__blog').on('mouseenter', function(){
@@ -26,7 +43,7 @@ Template.sidebar.rendered = function() {
           TweenMax.to($('.sidebar__blog span'), 0.5, {autoAlpha: 1, right: '-55px', delay: .55});
         });
 
-        $('.sidebar__geo').on('mouseenter', function(){
+        /*$('.sidebar__geo').on('mouseenter', function(){
           TweenMax.to($('.sidebar__geo__link__geo'), 0.5, {autoAlpha: 0});
           TweenMax.set($('.sidebar__geo__link__geo'), {top: 20,delay: .51});
           TweenMax.to($('.sidebar__geo__link__geo'), 0.5, {autoAlpha: 1,top: 0,delay: .70});
@@ -35,7 +52,7 @@ Template.sidebar.rendered = function() {
           TweenMax.set($('.sidebar__geo__link__subtitle'), {right: 0,delay: .56});
           TweenMax.to($('.sidebar__geo__link__subtitle'), 0.5, {autoAlpha: 1,right: 15,delay: .80});
 
-        });
+        });*/
       }
       linksAnimation();
     }
